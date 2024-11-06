099034e3b4a4093f9e41f909593e8d2899e32148
bonath <vannbonath1@gmail.com>
2024-11-06 11:25:12 +0700

    done time to integrate

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,15 @@ import { HttpExceptionFilter } from './filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalFilters(new HttpExceptionFilter()); // Apply the filter globally
+
+  // Enable CORS
+  app.enableCors({
+    origin: 'http://localhost:5174', // Replace with your frontend URL
+    methods: 'GET,POST,PUT,DELETE', // Define allowed methods
+    credentials: true, // Enable credentials (cookies, authorization headers)
+  });
+
+  app.useGlobalFilters(new HttpExceptionFilter()); // Apply the filter globally error
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
